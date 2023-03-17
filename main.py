3d12efbf636355f61a42f82c8c223052f4be4169
Abhinav Gupta <abhi.gupta8802@gmail.com>
2023-03-18 02:29:30 +0530

    added functionality to todo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("What would you like to do? ")
+    user_action = input("What would you like to do? add, show, edit, complete? ")
     user_action = user_action.strip()
 
     match user_action:
@@ -10,12 +10,16 @@ while True:
             todos.append(todo)
         case 'show':
             for index, item in enumerate(todos):
-                print(index + 1, ") " ,item)
+                row = f"{index + 1}. {item}"
+                print(row)
         case 'edit':
             number = int(input("Which todo would you like to edit? "))
             number -= 1
             new_todo = input("What would you like to change it to? ")
             todos[number] = new_todo
+        case 'complete':
+            number = int(input("What do you want to mark as complete? "))
+            todos.pop(number - 1)
         case 'exit':
             break
 
