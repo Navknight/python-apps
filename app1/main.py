0f576adbeceb2969f90088b54a93f35909a9a4fb
Navknight <abhi.gupta8802@gmail.com>
2023-03-23 19:32:49 +0530

    Added text file

--- app1/main.py
+++ app1/main.py
@@ -1,14 +1,25 @@
-todos = []
-
 while True:
     user_action = input("What would you like to do? add, show, edit, complete? ")
     user_action = user_action.strip()
 
     match user_action:
         case 'add':
-            todo = input("What would you like to add? ")
+            todo = input("What would you like to add?")
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
         case 'show':
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             for index, item in enumerate(todos):
                 row = f"{index + 1}. {item}"
                 print(row)
