e207ed435873c2e946d114a15ce0b73c51068def
Abhinav <abhi.gupta8802@gmail.com>
2023-04-04 06:40:46 +0530

    changed the get, write functions

--- app1/main.py
+++ app1/main.py
@@ -1,61 +1,70 @@
+def get_todos(filepath="todos.txt"):
+    with open(filepath, 'r') as file:
+        todos = file.readlines()
+    return todos
+
+def write_todos(todos, filepath="todos.txt"):
+    with open(filepath, 'w') as file:
+        file.writelines(todos)
+
 while True:
     user_action = input(
         "What would you like to do? add, show, edit, complete? ")
     user_action = user_action.strip()
 
-    match user_action:
-        case 'add':
-            todo = input("What would you like to add?") + "\n"
+    if user_action.startswith('add'):
+        todo = user_action[4:] + "\n"
 
-            with open('./todos.txt', 'r') as file:
-                todos = file.readlines()
+        todos = get_todos()
 
-            todos.append(todo)
+        todos.append(todo)
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+        write_todos(todos)
 
-        case 'show':
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+    elif user_action.startswith('show'):
+        todos = get_todos()
 
-            if len(todos) == 0:
-                print("Add todos to get started! ")
+        if len(todos) == 0:
+            print("Add todos to get started! ")
 
-            for index, item in enumerate(todos):
-                item = item.strip('\n')
-                row = f"{index + 1}-{item}"
-                print(row)
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index + 1}-{item}"
+            print(row)
 
-        case 'edit':
-            number = int(input("Which todo would you like to edit? "))
+    elif user_action.startswith('edit'):
+        try:
+            number = int(user_action[5:])
             number -= 1
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+            todos = get_todos()
 
             new_todo = input("What would you like to change it to? ")
             todos[number] = new_todo + "\n"
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos(todos)
+        except ValueError:
+            print("Enter a valid command! ")
 
-        case 'complete':
-            number = int(input("What do you want to mark as complete? ")) - 1
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+    elif user_action.startswith('complete'):
+        try:
+            number = int(user_action[9:])
+            number -= 1
 
-            while (number >= len(todos) or number < 0):
-                number = int(input("Enter a valid number! "))
-                number -= 1
+            todos = get_todos()
 
             todos.pop(number)
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos(todos)
+        except IndexError:
+            print("Enter a valif number! ")
+            continue
 
-        case 'exit':
-            break
+    elif user_action.startswith('exit'):
+        break
+
+    else:
+        print("enter a valid command! ")
 
 print('Bye!')
