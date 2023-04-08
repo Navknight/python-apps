be6302106d47359add71ea15809a7ee10c982302
navknight <abhi.gupta8802@gmail.com>
2023-04-08 18:01:16 +0530

    Render the app titles into columns

--- app2/main.py
+++ app2/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -9,6 +10,21 @@ with col1:
 
 with col2:
     st.title("Abhinav Gupta")
-    content = '''
-    Hi, I am Abhinav! I am a Computer Science and Engineering student from IIT Tirupati.'''
+    content = '''Hi, I am Abhinav! I am a Computer Science and Engineering student from IIT Tirupati.'''
     st.info(content)
+
+content2 = '''
+Below you can find some of the apps I have built in Python. Feel free to check them out'''
+
+st.write(content2)
+
+col3, col4 = st.columns(2)
+df = pandas.read_csv("images/data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
